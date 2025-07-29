a44ee888372feefaf1dfe9de5cb7eaf6cea90b7c
Chris White <cxwembedded@gmail.com>
2025-07-29 14:31:14 -0400

    Remap Alt from RAlt to LAlt
    For Godot and maybe also ctl+alt+del

--- examples/Devices/Keyboardio/Model100/Model100.ino
+++ examples/Devices/Keyboardio/Model100/Model100.ino
@@ -216,7 +216,7 @@ KEYMAPS(
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_Minus,     Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
 
-   Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
+   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
   [NUMPAD] =  KEYMAP_STACKED
