44aaf52b5aabde26a2e8ab4262a3f2aae03e7550
Chris White <cxwembedded@gmail.com>
2025-07-29 14:30:54 -0400

    sketch.mk: do not pass `-j 0`
    arduino-cli does not recognize it

--- etc/makefiles/sketch.mk
+++ etc/makefiles/sketch.mk
@@ -180,7 +180,7 @@ compile: kaleidoscope-hardware-configured check-rosetta
 	  --library "${KALEIDOSCOPE_DIR}" \
 	  --libraries "${KALEIDOSCOPE_DIR}/plugins/" \
 	  --build-path "${BUILD_PATH}" \
-	  --output-dir "${OUTPUT_PATH}" -j 0 \
+	  --output-dir "${OUTPUT_PATH}" \
 	  "${SKETCH_FILE_PATH}" $(if $(VERBOSE),,> /dev/null)
 ifeq ($(LIBONLY),)
 	$(QUIET) cp "${BUILD_PATH}/${SKETCH_FILE_NAME}.hex" "${HEX_FILE_PATH}"
